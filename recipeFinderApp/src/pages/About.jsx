0ae1f44da8d4c4f4a85a907d5197c0b8d9de9f55
hlina alemayoh <hilubecoder@gmail.com>
2025-01-09 22:27:38 +0300

    add the some style to the about page

--- recipeFinderApp/src/pages/About.jsx
+++ recipeFinderApp/src/pages/About.jsx
@@ -5,10 +5,10 @@ function About() {
     <div className="flex flex-col items-center justify-center md:pt-25 md:pb-28 py-16 max-w-screen-2xl text-center md:gap-14">
       <div className="p-2 text-button-yellow">
         <h2 className="text-3xl font-extrabold">About Us</h2>
-        <h3 className="text-hover-button font-bold text-lg">
+        <h3 className="text-hover-button font-bold text-lg p-2">
           Welcome to Recipe Finder!
         </h3>
-        <p className="lg:px-44 text-yellow-300 lg:text-xl md:text-lg  font-semibold">
+        <p className="lg:px-44 text-yellow-300 lg:text-xl md:text-md  font-semibold">
           At Recipe Finder, we believe that cooking should be easy, fun, and
           accessible to everyone. Whether you’re a seasoned chef or just
           starting your culinary journey, we’re here to inspire, guide, and
@@ -17,10 +17,10 @@ function About() {
       </div>
       <div className="p-2 text-button-yellow">
         <h2 className="text-3xl font-extrabold">Our Mission</h2>
-        <h4 className="text-hover-button font-bold text-md">
+        <h4 className="text-hover-button font-bold text-md p-2">
           Our mission is simple
         </h4>
-        <p className="lg:px-44 text-yellow-300 lg:text-xl md:text-lg font-semibold text-md">
+        <p className="lg:px-44 text-yellow-300 lg:text-xl md:text-md font-semibold text-md">
           To bring people together through the love of food. We aim to provide
           an extensive collection of recipes, from everyday meals to gourmet
           dishes, tailored to your taste and dietary preferences.
